ceec5e8b42d60a02f3b231955f58159168873b7d
Hayan.Shamas <Hayan.Shamas@gmail.com>
2018-12-17 13:08:51 -0500

    Update, Remove Documentum Reference

--- connect/Service/ibm/cm/filter/HangFireCustomAutoRetryJobFilterAttribute.cs
+++ connect/Service/ibm/cm/filter/HangFireCustomAutoRetryJobFilterAttribute.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace connect.Service.documentum.filter
+namespace connect.Service.ibm.cm.filter
 {
     public class HangFireCustomAutoRetryJobFilterAttribute : JobFilterAttribute, IElectStateFilter
     {
